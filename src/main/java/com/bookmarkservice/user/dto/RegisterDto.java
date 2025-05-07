a878a2001ad500a0f6e94990b5ed8f67d14d0791
decollozq <decollzoq@gmail.com>
2025-05-07 20:30:28 +0900

    refactor/register -1: 도메인 이름 변경

--- src/main/java/com/bookmarkservice/user/dto/RegisterDto.java
+++ src/main/java/com/bookmarkservice/user/dto/RegisterDto.java
@@ -7,7 +7,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class SignupRequestDto {
+public class RegisterDto {
     private String email;
     private String password;
     private String nickname;
